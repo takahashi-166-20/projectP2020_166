f9b4c57f67b46924a2480f7dc78dc53b78a81827
takahashi-166-20 <c01181667a@edu.teu.ac.jp>
2020-05-29 18:51:22 +0900

    課題2を修正しました

--- ex1/php2.php
+++ ex1/php2.php
@@ -1,13 +1,11 @@
 
         <?php
         $addresses = array(['name'=>'東京太郎','address'=>'東京都','phone'=>'012-345-6789','email'=>'taro@example.com'],['name'=>'工科花子','address'=>'北海道','phone'=>'987-654-3210','email'=>'hana@example@.com']);
-        //print_table($addresses);
-        $json = json_encode($addresses,JSON_UNESCAPED_UNICODE);
-        print_r($json);
+        print_table($addresses);
         function print_table($addresses){
             printf("<table border=1 width=500>");
             printf("<tr>");
-            printf("<td>名前</td><td>住所</td><td>電話</td><td>Email</td>");
+            printf("<th>名前</th><th>住所</th><th>電話</th><th>Email</th>");
             printf("</tr>");
             foreach($addresses as $key) {
                 foreach($key as $value){
@@ -21,4 +19,5 @@
         }
 
         ?>
+        
         
