d1adfd502c372713b03d1dbaad8efce39a4bd08e
takahashi-166-20 <c01181667a@edu.teu.ac.jp>
2020-06-05 14:54:34 +0900

    php3再修正しました

--- ex1/php3.php
+++ ex1/php3.php
@@ -1,18 +1,33 @@
+<!DOCTYPE html>
+<html lang = "ja">
+<head>
+    <meta charset="UTF-8">
+    <title>php3</title>
+    <style>
+  table, tr, th, td {
+    border: 1px solid #000;
+  }
+</style>
+</head>
+<body>
 <?php
         $addresses = array(['name'=>'東京太郎','address'=>'東京都','phone'=>'012-345-6789','email'=>'taro@example.com'],['name'=>'工科花子','address'=>'北海道','phone'=>'987-654-3210','email'=>'hana@example@.com']);
         $addresses[] = get_line();
         print_table($addresses);
         
         function print_table($addresses){
-            printf("<table border=1 width=500>");
+            printf("<table>");
             printf("<tr>");
             printf("<th>名前</th><th>住所</th><th>電話</th><th>Email</th>");
             printf("</tr>");
             foreach($addresses as $key) {
-                foreach($key as $value){
-                    printf("<td>".$value."</td>");
-                }
-                printf("<tr></tr>");
+                printf("<tr>");
+                printf("<td>".$key['name']."</td>");
+                printf("<td>".$key['address']."</td>");
+                printf("<td>".$key['phone']."</td>");
+                printf("<td>".$key['email']."</td>");
+                printf("</tr>");
+                
             }
             printf("</table>");
         }
@@ -31,9 +46,10 @@
             
             function get_line(){
             if($_POST != null){
-                $nline = array('名前'=>$_POST['name'],'住所'=>$_POST['address'],'電話番号'=>$_POST['phone'],'メール'=>$_POST['email']);
+                $nline = array('name'=>$_POST['name'],'address'=>$_POST['address'],'phone'=>$_POST['phone'],'email'=>$_POST['email']);
                 return $nline;
             }
             else return null;
         }
         ?>
+        </body>
