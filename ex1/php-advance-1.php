40682748da1d862c222c688b5dced08bd349bcb6
takahashi-166-20 <c01181667a@edu.teu.ac.jp>
2020-06-12 17:50:29 +0900

    応用課題1完成しました

---
+++ ex1/php-advance-1.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang = "ja">
+<?php
+    
+?>
+<head>
+    <meta charset="UTF-8">
+    <title>php-advance1</title>
+    <style>
+  table, tr, th, td {
+    border: 1px solid #000;
+  }
+  
+</style>
+</head>
+<?php
+    session_start();
+    if($_SESSION['accsess']==null){
+        printf("初めての訪問です！");
+        $_SESSION['accsess'] += 1;
+    }
+    else{
+        $_SESSION['accsess'] += 1;
+        print_acsess();
+    }
+    
+    function print_acsess(){
+        $acsess = (int)0;
+        $acsess += $_SESSION['accsess'];
+        print("<th>");
+        print("".$acsess."回目の訪問です！</th>");
+        
+    } 
+?>
