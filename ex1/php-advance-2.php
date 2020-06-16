5ba2a22fc83b51bb9a849108fedc04cf6adfb884
takahashi-166-20 <c01181667a@edu.teu.ac.jp>
2020-06-16 18:09:41 +0900

    php応用課題2完成しました

---
+++ ex1/php-advance-2.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang = "ja">
+<?php
+    
+?>
+<head>
+    <meta charset="UTF-8">
+    <title>php-advance2</title>
+    <style>
+  table, tr, th, td {
+    border: 1px solid #000;
+  }
+  
+</style>
+</head>
+<body>
+    <?php
+     if($_POST != null){
+         //print("Successful:ファイルのアップロードに成功しました！");
+         get_file();
+     }
+     else{
+         //print( "Error:ファイルが選択されていません。");
+     }
+    function get_file(){
+        if (is_uploaded_file($_FILES["select_file"]["tmp_name"])) {
+            if(move_uploaded_file($_FILES["select_file"]["tmp_name"], $_FILES["select_file"]["name"])) {
+                chmod( $_FILES["select_file"]["name"], 0644);
+                print("Successful:ファイルのアップロードに成功しました！");
+            }
+        } else {
+            print( "Error:ファイルが選択されていません。");
+        }
+    }
+    ?>
+    <form method="POST" action="php-advance-2.php" enctype = "multipart/form-data">
+        <p>ファイルアップローダー</p>
+        <input type="file" name="select_file" >
+        <input type="submit" name="button1" id="button1" value="送信">
+    </form>
+    
+</body>
