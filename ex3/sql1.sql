297cd7df5be18c3c1876683abab8d43de4990317
takahashi-166-20 <c01181667a@edu.teu.ac.jp>
2020-07-10 17:08:04 +0900

    SQL1修正しました

--- ex3/sql1.sql
+++ ex3/sql1.sql
@@ -8,5 +8,5 @@ CREATE TABLE addresses(
 );
 INSERT INTO addresses VALUE(1,'工科 太郎', '八王子', '012-345-6789','tarou@exsample.com');
 INSERT INTO addresses VALUE(2,'蒲田 花子', '蒲田', '987-654-3210', 'hanako@example.com');
-SELECT * FROM addresses
-SELECT * FROM addresses WHERE address = "八王子"
+SELECT * FROM addresses;
+SELECT * FROM addresses WHERE address = "八王子";
