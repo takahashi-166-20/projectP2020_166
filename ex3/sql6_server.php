996b598c3d1a2992b9a180c38810845738526bc5
takahashi-166-20 <c01181667a@edu.teu.ac.jp>
2020-07-29 21:11:54 +0900

    SQL5で修正した箇所を反映させました

--- ex3/sql6_server.php
+++ ex3/sql6_server.php
@@ -14,10 +14,16 @@
     
     if(isset($_GET["chg_id"])&&isset($_GET["chg_name"])&&isset($_GET["chg_address"])&&isset($_GET["chg_phone"])&&isset($_GET["chg_email"])){
         $id = $_GET["chg_id"];
-        $name = $_GET["chg_name"];
-        $address = $_GET["chg_address"];
-        $phone = $_GET["chg_phone"];
-        $email = $_GET["chg_email"];
+        $statement = $pdo -> query("SELECT * FROM addresses WHERE id LIKE '%$id%'");
+        $results = $statement->fetchAll();
+        if($_GET["chg_name"] == "") $name=$results[0]["name"];
+        else $name = $_GET["chg_name"];
+        if($_GET["chg_address"] == "") $address=$results[0]["address"];
+        else $address = $_GET["chg_address"];
+        if($_GET["chg_phone"] == "") $phone=$results[0]["phone"];
+        else $phone = $_GET["chg_phone"];
+        if($_GET["chg_email"] == "") $email=$results[0]["email"];
+        else $email = $_GET["chg_email"];
         $pdo -> query("UPDATE addresses SET `name`='$name',`address`='$address',`phone`='$phone',`email`='$email' WHERE id ='$id';");
     }
     if(isset($_GET["del_id"])){
