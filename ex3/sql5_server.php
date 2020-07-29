e4624ba4cec245c83fafd556973de25bedabf330
takahashi-166-20 <c01181667a@edu.teu.ac.jp>
2020-07-29 20:33:38 +0900

    SQL5修正しました

--- ex3/sql5_server.php
+++ ex3/sql5_server.php
@@ -14,16 +14,29 @@
     
     if(isset($_GET["chg_id"])&&isset($_GET["chg_name"])&&isset($_GET["chg_address"])&&isset($_GET["chg_phone"])&&isset($_GET["chg_email"])){
         $id = $_GET["chg_id"];
-        $name = $_GET["chg_name"];
-        $address = $_GET["chg_address"];
-        $phone = $_GET["chg_phone"];
-        $email = $_GET["chg_email"];
+        $statement = $pdo -> query("SELECT * FROM addresses WHERE id LIKE '%$id%'");
+        $results = $statement->fetchAll();
+        if($_GET["chg_name"] == "") $name=$results[0]["name"];
+        else $name = $_GET["chg_name"];
+        if($_GET["chg_address"] == "") $address=$results[0]["address"];
+        else $address = $_GET["chg_address"];
+        if($_GET["chg_phone"] == "") $phone=$results[0]["phone"];
+        else $phone = $_GET["chg_phone"];
+        if($_GET["chg_email"] == "") $email=$results[0]["email"];
+        else $email = $_GET["chg_email"];
         $pdo -> query("UPDATE addresses SET `name`='$name',`address`='$address',`phone`='$phone',`email`='$email' WHERE id ='$id';");
+        // $json = json_encode($results,JSON_UNESCAPED_UNICODE);
+        // print($json);
+        // exit;
     }
 
     if(isset($_GET["slct_name"])){
         $name = $_GET["slct_name"];
         $statement = $pdo -> query("SELECT * FROM addresses WHERE name LIKE '%$name%'");
+    }
+    elseif(isset($_GET["slct_id"])){
+        $id = $_GET["slct_id"];
+        $statement = $pdo -> query("SELECT * FROM addresses WHERE id LIKE '%$id%'");
     }
     else $statement = $pdo -> query("SELECT * FROM addresses");
     $results = $statement->fetchAll();
